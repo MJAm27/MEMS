2b16a0f604e364bdb36c2657e2c69d6c98e163cd
MJAm27 <jinhwan7ikon@gmail.com>
2026-01-27 14:20:12 +0700

    ปรับตำเเหน่งการวาง

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,16 @@
 import React from 'react';
-    import { BrowserRouter, Routes, Route } from 'react-router-dom';
-    import LoginPage from './components/LoginPage';
-    import VerifyPage from './components/VerifyPage';
-    import Setup2FAPage from './components/Setup2FAPage';
-    import DashboardPage from './components/DashboardPage';
-    import RegisterPage from './components/RegisterPage';
-<<<<<<< HEAD
-    import InventoryBalanceReportChart from './components/InventoryBalanceReportChart'
-    //import LowStockAlert from './components/LowStockAlert'
-    import UserList from './components/UserList'
-    import EditUser from './components/EditUser'
-    
-=======
-    import ProfileEditENG from './components/Profile'
-    import InventoryBalanceReportChart from './components/InventoryBalanceReportChart';
->>>>>>> 952a636d28d4d48590e7b09e416970bf25bba5ea
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import LoginPage from './components/LoginPage';
+import VerifyPage from './components/VerifyPage';
+import Setup2FAPage from './components/Setup2FAPage';
+import DashboardPage from './components/DashboardPage';
+import RegisterPage from './components/RegisterPage';
+
+import InventoryBalanceReportChart from './components/InventoryBalanceReportChart';
+import UserList from './components/UserList';
+import EditUser from './components/EditUser';
+import ProfileEditENG from './components/Profile';
+
 
     function App() {
         return (
@@ -30,13 +26,12 @@ import React from 'react';
                     <Route path="/profileEditENG" element={<ProfileEditENG />} /> 
                     <Route path="/report/inventory-balance" element={<InventoryBalanceReportChart />} />
 
-<<<<<<< HEAD
+
                     <Route path='/inventoryBalanceReportChart' element={<InventoryBalanceReportChart />}/>
 
                     <Route path='/userList' element={<UserList />}/>
                     <Route path='/editUser' element={<EditUser />}/>
-=======
->>>>>>> 952a636d28d4d48590e7b09e416970bf25bba5ea
+
                     
                 </Routes>
             </BrowserRouter>
