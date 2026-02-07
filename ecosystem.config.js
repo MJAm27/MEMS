9e058416900bc6daf9552085257225d7d25bf2e2
MJAm27 <jinhwan7ikon@gmail.com>
2026-02-07 22:36:56 +0700

    ..

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,10 +1,5 @@
 module.exports = {
     apps: [{
-        name: "mems-client",
-        cwd: "./client",
-        script: "npm",
-        args: "start"
-    }, {
         name: "mems-server",
         cwd: "./server",
         script: "npm",
