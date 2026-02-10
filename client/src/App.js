0bde3a62fb46b4d23c95039b46307a794e7124cf
MJAm27 <jinhwan7ikon@gmail.com>
2026-02-10 14:32:40 +0700

    ปรับการออกจากระบบ

--- client/src/App.js
+++ client/src/App.js
@@ -1,44 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LoginPage from './components/LoginPage';
+import RegisterPage from './components/RegisterPage';
 import VerifyPage from './components/VerifyPage';
 import Setup2FAPage from './components/Setup2FAPage';
-import DashboardPage from './components/DashboardPage';
-import RegisterPage from './components/RegisterPage';
-
-import InventoryBalanceReportChart from './components/InventoryBalanceReportChart';
-import EquipmentUsageReportChart from './components/EquipmentUsageReportChart';
-import EquipmentAgeReportChart from './components/EquipmentAgeReportChart';
-import UserList from './components/UserList';
-import ProfileEditENG from './components/Profile';
 import ManagerMainPage from './components/ManagerMainPage';
+import DashboardPage from './components/DashboardPage';
 
+function App() {
+    const [user] = useState(JSON.parse(localStorage.getItem('user')) || null);
 
+    const handleLogout = () => {
+        localStorage.removeItem('token');
+        localStorage.removeItem('user');
+        window.location.href = "/login";
+    };
 
-    function App() {
-        return (
-            <BrowserRouter>
-                <Routes>
-                    <Route path="/" element={<LoginPage />} />
-                    <Route path="/login" element={<LoginPage />} />
-                    <Route path="/register" element={<RegisterPage />} /> 
-                    <Route path="/verify" element={<VerifyPage />} />
-                    <Route path="/setup-2fa" element={<Setup2FAPage />} />
-                    <Route path="/dashboard/manager/*" element={<ManagerMainPage />} />
-                    <Route path="/dashboard/*" element={<DashboardPage />} /> 
-                    <Route path="/profileEditENG" element={<ProfileEditENG />} /> 
-                    <Route path="/report/inventory-balance" element={<InventoryBalanceReportChart />} />
-                    <Route path="/report/equipment-usage" element={<EquipmentUsageReportChart />} />
-                    <Route path="/report/equipment-age" element={<EquipmentAgeReportChart />} />
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<LoginPage />} />
+                <Route path="/login" element={<LoginPage />} />
+                <Route path="/register" element={<RegisterPage />} /> 
+                <Route path="/verify" element={<VerifyPage />} />
+                <Route path="/setup-2fa" element={<Setup2FAPage />} />
+                {/* ส่ง handleLogout เป็น Prop ไปให้ Manager */}
+                <Route 
+                    path="/dashboard/manager/*" 
+                    element={<ManagerMainPage user={user} handleLogout={handleLogout} />} 
+                />
+                <Route path="/dashboard/*" element={<DashboardPage />} /> 
+            </Routes>
+        </BrowserRouter>
+    );
+}
 
-                    <Route path='/userList' element={<UserList />}/>
-
-                    
-                </Routes>
-            </BrowserRouter>
-        );
-    }
-
-    export default App;
-
-                        //<Route path='/lowStockAlert' element={<LowStockAlert />}/>
+export default App;
